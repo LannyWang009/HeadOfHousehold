af686d1544ea55e5f6045e6d2520a783cccd4bdc
Audy Arandela <audyarandela@gmail.com>
2018-11-16 20:58:45 -0600

    database function, ran travis CI

--- js/database.js
+++ js/database.js
@@ -1,23 +1,23 @@
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAfOH62OimyZABipB7qopRwtbbwuNdKGaA",
-    authDomain: "headofhousehold-222605.firebaseapp.com",
-    databaseURL: "https://headofhousehold-222605.firebaseio.com",
-    projectId: "headofhousehold-222605",
-    storageBucket: "headofhousehold-222605.appspot.com",
-    messagingSenderId: "672191560403"
-};
-firebase.initializeApp(config);
+  apiKey: 'AIzaSyAfOH62OimyZABipB7qopRwtbbwuNdKGaA',
+  authDomain: 'headofhousehold-222605.firebaseapp.com',
+  databaseURL: 'https://headofhousehold-222605.firebaseio.com',
+  projectId: 'headofhousehold-222605',
+  storageBucket: 'headofhousehold-222605.appspot.com',
+  messagingSenderId: '672191560403'
+}
+firebase.initializeApp(config)
 
 // Initialize Cloud Firestore through Firebase
 var firestore = firebase.firestore()
 
 // Disable deprecated features
 const settings = {
-    timestampsInSnapshots: true
-};
-firestore.settings(settings);
+  timestampsInSnapshots: true
+}
+firestore.settings(settings)
 
 const docRef = firestore.doc('tasks/taskType')
 
@@ -25,46 +25,43 @@ const inputTaskField = document.getElementById('tasks')
 const saveButton = document.getElementById('saveTask')
 const outputTask = document.getElementById('taskOutput')
 
-
 saveButton.addEventListener('click', function () {
-    const textToSave = inputTaskField.value
-    console.log('Saved: ' + textToSave + ' to Firestore')
-    docRef.set({
-        task: textToSave
-    }).then(function () {
-        console.log('Status saved')
-    }).catch(function (error) {
-        console.log('Got an error', error)
-    })
+  const textToSave = inputTaskField.value
+  console.log('Saved: ' + textToSave + ' to Firestore')
+  docRef.set({
+    task: textToSave
+  }).then(function () {
+    console.log('Status saved')
+  }).catch(function (error) {
+    console.log('Got an error', error)
+  })
 })
 
-
 function getRealtimeUpdates () {
-    docRef.onSnapshot(function (doc) {
-        if (doc && doc.exists) {
-            const taskData = doc.data()
-            console.log("Document received: ", doc)
-            outputTask.innerText = "Task: " + taskData.task
-        }
-    })
+  docRef.onSnapshot(function (doc) {
+    if (doc && doc.exists) {
+      const taskData = doc.data()
+      console.log('Document received: ', doc)
+      outputTask.innerText = 'Task: ' + taskData.task
+    }
+  })
 }
 
 getRealtimeUpdates()
 
-
-
-function autoComplete() {
-    var availableTasks = [
-        'Dishes',
-        'Laundry',
-        'Call',
-        'Groceries',
-        'Yardwork',
-        'Oil change',
-        'Homework',
-        'Walk Dog'
-    ];
-    $("#task").autocomplete({
-        source: availableTasks
-    });
+// Trying to use an autocomplete function using jQueryUI, still needs work.
+function autoComplete () {
+  var availableTasks = [
+    'Dishes',
+    'Laundry',
+    'Call',
+    'Groceries',
+    'Yardwork',
+    'Oil change',
+    'Homework',
+    'Walk Dog'
+  ]
+  $('#task').autocomplete({
+    source: availableTasks
+  })
 };
